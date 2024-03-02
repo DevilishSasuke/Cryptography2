82c1d442d3f8434f66dca1b5148ffd2c3e2a8c1d
DevilishSasuke <108042884+DevilishSasuke@users.noreply.github.com>
2024-03-02 18:09:36 +0300

    added already released functions

---
+++ Cryptography2/EratosthenesSieve.cs
@@ -0,0 +1,35 @@
+﻿namespace Cryptography
+{
+    public static class EratosthenesSieve
+    {
+        private static int previousN;
+        private static int[] sieve;
+        public static int[] GetSieve(int n = 5000)
+        {
+            if (n < 1)
+                n = 5000;
+            if (sieve != null && previousN == n)
+                return sieve;
+            previousN = n;
+
+            var methodSieve = new int[n];
+            for (int i = 0; i < methodSieve.Length; ++i)
+                methodSieve[i] = i;
+
+            for (int j = 2; j < methodSieve.Length; ++j)
+            {
+                if (methodSieve[j] == 0) continue;
+                for (int k = j * j; k < methodSieve.Length; k += j)
+                    methodSieve[k] = 0;
+            }
+
+            var primeList = new List<int>();
+            for (int i = 2; i < methodSieve.Length; ++i)
+                if (methodSieve[i] != 0)
+                    primeList.Add(methodSieve[i]);
+
+            sieve = primeList.ToArray();
+            return sieve;
+        }
+    }
+}
---
+++ Cryptography2/PrimeGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Numerics;
+
+namespace Cryptography
+{
+    public class PrimeGenerator
+    {
+        private readonly Random random = new Random();
+        private BigInteger candidate { get; set; }
+        public BigInteger GeneratePrime()
+        {
+            int k = 50;
+
+            while (true)
+            {
+                candidate = randNum();
+                if (candidate == 0) continue;
+
+                if (IsLikelyPrime(candidate))
+                    if (RabinMiller(candidate, k))
+                        return candidate;
+            }
+
+        }
+
+        public BigInteger randNum(int len = 0)
+        {
+            len = len > 0 ? len / 8 : random.Next(100, 130) / 8;
+            var bytes = new byte[len];
+
+            random.NextBytes(bytes);
+            bytes[bytes.Length - 1] &= (byte)0x7F;
+
+            return new BigInteger(bytes);
+        }
+
+        public static bool IsLikelyPrime(BigInteger number)
+        {
+            var sieve = EratosthenesSieve.GetSieve();
+
+            foreach (var prime in sieve)
+            {
+                if (prime >= number)
+                    return true;
+                if (number % prime == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool RabinMiller(BigInteger number, int steps)
+        {
+            var b = number - 1;
+            var k = -1;
+            BigInteger d, x;
+            BigInteger[] beta = new BigInteger[150];
+            do
+            {
+                k++;
+                beta[k] = b % 2;
+                b = b / 2;
+            } while (b > 0);
+
+            for (int j = 0; j < steps; j++)
+            {
+                var a = randNum();
+                if (CryptoRSA.EuclidExtended(a, number) > 1)
+                    return false;
+                d = 1;
+                for (int i = k; i >= 0; i--)
+                {
+                    x = d;
+                    d = (d * d) % number;
+                    if (d == 1 && x != 1 && x != number - 1)
+                        return false;
+                    if (beta[i] == 1)
+                        d = (d * a) % number;
+                }
+                if (d != 1)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
